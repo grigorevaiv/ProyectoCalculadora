c29e459b9a584de304995a71bff745fd53876973
grigorevaiv <79690005+grigorevaiv@users.noreply.github.com>
2024-02-12 10:33:29 -0500

    Updated CalculadoraPrueba.java

--- CalculadoraPrueba.java
+++ CalculadoraPrueba.java
@@ -3,5 +3,17 @@ public class CalculadoraPrueba {
         Calculadora calc = new Calculadora(5, 3, "suma");
         calc.calcular();
         System.out.println(calc.getResultado());
+        
+        calc = new Calculadora(5, 3, "resta");
+        calc.calcular();
+        System.out.println(calc.getResultado());
+
+        calc = new Calculadora(5, 3, "multiplicar");
+        calc.calcular();
+        System.out.println(calc.getResultado());
+
+        calc = new Calculadora(5, 3, "dividir");
+        calc.calcular();
+        System.out.println(calc.getResultado());
     }
 }
